25c1eced24c2f55e24df9d82b40ed084f311defd
koinu1203 <daniel_ortizpary@hotmail.com>
2020-07-04 22:52:02 -0500

    v2

---
+++ ProyectAsimov/ProyectAsimov/Clases/M_Eno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAsimov.Clases
+{
+    class M_Eno
+    {
+        Matriz m;
+
+        public M_Eno(Matriz m)
+        {
+            this.m = m;
+        }
+    }
+}
---
+++ ProyectAsimov/ProyectAsimov/Clases/Matriz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAsimov.Clases
+{
+    class Matriz
+    {
+        private int x { get; set; }
+        private int y { get; set; }
+        private int[,] mp { get; set; }
+        private int[,] ms { get; set; }
+        private bool[,] mb { get; set; }
+        private int[] lineaX { get; set; }
+        private int[] lineaY { get; set; }
+
+        public Matriz(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            crearmatriz();
+        }
+
+        private void crearmatriz()
+        {
+            ms = new int[x, y];
+            mp = new int[x, y];
+            mb = new bool[x.y];
+            lineaX = new int[x];
+            lineaY = new int[y];
+        }
+
+    }
+}

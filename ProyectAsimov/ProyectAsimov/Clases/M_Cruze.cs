be525123930dac5b2c0af3bddfcba597fbb8683c
koinu1203 <daniel_ortizpary@hotmail.com>
2020-07-08 11:03:55 -0500

    v8

---
+++ ProyectAsimov/ProyectAsimov/Clases/M_Cruze.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAsimov.Clases
+{
+    class M_Cruze
+    {
+        private M_Eno m;
+        public M_Cruze(M_Eno m)
+        {
+            this.m = m;
+        }
+
+    }
+}
